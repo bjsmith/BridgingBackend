11cea91b7346416f438846f6162d68b608539bec
Ben J. Smith <benjsmith@gmail.com>
2024-10-24 22:18:34 -0700

    update comments on SQL

--- retrieve_videos.sql
+++ retrieve_videos.sql
@@ -6,20 +6,19 @@ WITH selected_genres AS (
     LIMIT 5  -- Change this number to limit how many genres you want to randomly select
 ),
 all_videos_randomized AS(
-    -- Step 2: Randomize the order of all the videos present
+    -- Step 2: Randomize the order of all videos with an access count under the threshold
 	SELECT * 
 	FROM videos v
 	WHERE v.access_count <= 10
 	ORDER BY v.genre_id, RANDOM()
 ),
 selected_videos AS (
-    -- Step 3: For each selected genre, pick one random video with access_count <= 10
+    -- Step 3: For each selected genre, pick one video from the set above
     SELECT DISTINCT ON (v.genre_id) v.video_id, v.genre_id, v.access_count
     FROM all_videos_randomized v
 	--must use inner join to select just the videos associated with the selected genres
     INNER JOIN selected_genres sg ON v.genre_id = sg.genre_id 
 	ORDER BY v.genre_id
-	--does the order by work correctly?
 ),
 updated_videos AS (
     -- Step 4: Update the access_count for each selected video
@@ -28,7 +27,7 @@ updated_videos AS (
     WHERE video_id IN (SELECT video_id FROM selected_videos)
     RETURNING video_id, video_creator_username, video_site_id, genre_id -- Return the updated records
 )
--- Step 4: Return the selected videos with their updated access_count
+-- Step 4: Return the selected videos with the genre title for each video merged from the genres table
 SELECT v.video_id, v.video_creator_username, v.video_site_id, ge.title AS genre_title FROM updated_videos v
 LEFT JOIN genres ge
 on v.genre_id=ge.genre_id
